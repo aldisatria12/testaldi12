082d5c2988f4d07829a2e8ab6cbc7d304d7ea492
aldisatria12 <56900446+aldisatria12@users.noreply.github.com>
2020-12-14 08:23:48 +0700

    initial commit

---
+++ week144444/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace week144444
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka = 123;
+            string angka2 = angka.ToString();
+            string nama = "aldi";
+            string nama2 = nama + "satria";
+            string nama3 = string.Concat(nama, nama2, angka2);
+            string nama4 = nama3.Substring(3, 5);
+            Console.WriteLine(nama4[1]);
+        }
+        
+    }
+}

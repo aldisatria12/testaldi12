3a610ab49f5bb5ca3ccf8ee7bee955ea6e89bad1
aldisatria12 <56900446+aldisatria12@users.noreply.github.com>
2020-12-14 08:41:48 +0700

    test

--- week144444/Program.cs
+++ week144444/Program.cs
@@ -9,7 +9,7 @@ namespace week144444
             int angka = 123;
             string angka2 = angka.ToString();
             string nama = "aldi";
-            string nama2 = nama + "satria";
+            string nama2 = nama + "mahotma";
             string nama3 = string.Concat(nama, nama2, angka2);
             string nama4 = nama3.Substring(3, 5);
             Console.WriteLine(nama4[1]);
